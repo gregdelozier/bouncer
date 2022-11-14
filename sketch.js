c4a5f055b7bf7f1f6829d952ec0c30670e404d53
Timothy Sanford II <109392462+timothySanfordII@users.noreply.github.com>
2022-11-14 19:54:49 +0000

    Updated Canvas size

--- sketch.js
+++ sketch.js
@@ -4,19 +4,20 @@ var dy = 2.0
 var x = 0
 var y = 0
 
+//updated canvas size - Tim
 function setup() {
   x = 0
   y = 0
-  createCanvas(400, 400);
+  createCanvas(500, 500);
 }
 
 function draw() {
   x = x + dx
   y = y + dy
-  if ((x > 400) | (x < 0)) {
+  if ((x > 500) | (x < 0)) {
     dx = -dx    
   }
-  if ((y > 400) | (y < 0)) {
+  if ((y > 500) | (y < 0)) {
     dy = -dy    
   }
   background(255, 160, 122);
